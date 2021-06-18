d952d2e14d3ca57860e5322b4c41ec98cb3bf050
Nataliia Kirinovich <kirinovich.natasha@gmail.com>
2021-06-18 13:25:38 +0300

    correction of comments

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import { createServer, IncomingMessage, ServerResponse } from "http";
 import fs from "fs";
 import { parse, ParsedQuery } from "query-string";
+import variables from "./infrastructureVariables";
 
-const port = 4000;
+const getNameWithCapitalLetter = (name: string): string => {
+  return name[0].toUpperCase() + name.slice(1);
+};
 
-const isPerson = (obj: any): boolean => {
-  return obj.name.length > 0 && Date.parse(obj.date) > 0;
+const getAgeByDateOfBirth = (date: string): number => {
+  return new Date().getFullYear() - new Date(date).getFullYear();
 };
 
 createServer((req: IncomingMessage, res: ServerResponse): void => {
@@ -15,41 +18,31 @@ createServer((req: IncomingMessage, res: ServerResponse): void => {
     "Access-Control-Allow-Headers",
     "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
   );
-  switch (req.method) {
-    case "POST":
-      let body = "";
-      req.on("data", (chunk) => {
-        body += chunk.toString();
-      });
-      req.on("end", () => {
-        const person: ParsedQuery<string> = parse(body);
-        if (
-          typeof person.date === "string" &&
-          typeof person.name === "string" &&
-          isPerson(person)
-        ) {
-          const name = person.name[0].toUpperCase() + person.name.slice(1);
-          const age =
-            new Date().getFullYear() - new Date(person.date).getFullYear();
-          const hello = `Hello! My name is ${name}! This year I am ${age} years old\n`;
-          fs.appendFile("data.txt", hello, () => {
-            res.statusCode = 200;
-            res.end(`Data has been successfully written to file`);
-          });
-        } else {
-          res.statusCode = 400;
-          res.statusMessage = "Bad Request";
-          res.end(`Error ${res.statusCode}. ${res.statusMessage}`);
-        }
-      });
-      break;
-    case "OPTIONS":
-      res.statusCode = 200;
-      res.end(`ok`);
-      break;
-    default:
-      res.statusCode = 501;
-      res.statusMessage = "Not Implemented";
-      res.end(`Error ${res.statusCode}. ${res.statusMessage}`);
+  if (req.method != "POST") {
+    return res.writeHead(403, "Forbidden").end(`Error 403. Forbidden`);
   }
-}).listen(4000, () => console.log(`The server was opened on port ${port}`));
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    const person: ParsedQuery<string> = parse(body);
+    if (
+      !person.date ||
+      !person.name ||
+      isNaN(Date.parse(person.date.toString()))
+    ) {
+      return res.writeHead(400, "Bad Request").end(`Error 400. Bad Request`);
+    }
+    const name = getNameWithCapitalLetter(person.name.toString());
+    const age = getAgeByDateOfBirth(person.date.toString());
+    const hello = `Hello! My name is ${name}! This year I am ${age} years old\n`;
+    fs.appendFile("data.txt", hello, () => {
+      res
+        .writeHead(200, "Ok")
+        .end(`Data has been successfully written to file`);
+    });
+  });
+}).listen(4000, () =>
+  console.log(`The server was opened on port ${variables.port}`)
+);
